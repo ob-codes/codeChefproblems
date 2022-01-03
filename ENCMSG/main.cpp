8351ad48939472c23e4623cf25a13b9357b6981f
owaisbukhari00 <owaisbukhari@outlook.com>
2022-01-03 17:49:08 +0800

    test cases handled successful - submission giving runtime error

--- ENCMSG/main.cpp
+++ ENCMSG/main.cpp
@@ -2,6 +2,26 @@
 #include <string>
 using namespace std;
 
+string interchange(string str)
+{
+	for (int i=0;i<str.size();i++)
+	{
+	    if (str[i] < 'a' || str[i] > 'z') return "Error";
+		
+		if (i%2!=0)
+	    {
+	        swap(str[i-1], str[i]);
+			/*
+			char temp;
+	        temp = str[i-1];
+	        str[i-1]=str[i];
+	        str[i]=temp;
+			*/
+	    }	        
+	}
+	return str;
+}
+
 void encode(string a)
 {
 	int temp;
@@ -9,8 +29,12 @@ void encode(string a)
 
 	for (int i=0;i<a.size();i++)
 	{
+		/*
 		temp = 109 - (int)a[i];
 		res[i] = (char)(temp*2 +1 + (int)a[i]);
+		*/
+		temp = 97 + 122 - (int)a[i];
+		res[i] = (char) temp;		
 		cout << res[i];
 	}
 	cout << endl;
@@ -19,24 +43,18 @@ void encode(string a)
 int main() 
 {
 	// your code goes here
-	int t, len;
+	int t;
 	cin >> t;
-	cin >> len;
+
 	while(t--)
 	{
-	    string str;
-	    cin >> str;
-		for (int i=0;i<len;i++)
-	    //for (int i=0;i<str.size();i++)
-	    {
-	        if (i%2!=0)
-	        {
-	            char temp;
-	            temp = str[i-1];
-	            str[i-1]=str[i];
-	            str[i]=temp;
-	        }	        
-	    }
+	    int len;
+		string str;
+	
+		cin >> len >> str;
+
+		str = interchange(str);
+		
 		encode(str);
 	}
 	return 0;
