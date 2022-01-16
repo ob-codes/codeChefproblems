289e22e109e50659ab771311609a370146267fd8
owaisbukhari00 <owaisbukhari@outlook.com>
2022-01-16 14:19:57 +0800

    TLE and WA

--- CIRCLEGAME/main.cpp
+++ CIRCLEGAME/main.cpp
@@ -10,32 +10,26 @@ int main()
 	
 	while (t--)
     {
-        unsigned long m, x;
+        int m, x;
         cin >> m >> x;
+        m--;
 
         vector <int> tempvec;
-        for (int n = 1; n <= x; n++)
+        for (int n = 1; n <= x; n++) //N=1 to N=X
         {
-            if(n==1) {cout << 1 << ' '; continue;}
-            for(int i=1; i <= n;i++)
+            if(n==1) {cout << n << ' '; continue;}
+            for(int i=1; i <= n;i++) //assign number to each index
             {
                 tempvec.push_back(i);
             }
-            for (int j = 1; j <= n-1; j++)
+            for (int j = 1; j <= n-1; j++) //execute N-1 times
             {
-                vector<int>::iterator index = tempvec.begin();
-                index += m-1;
-                /*
-                for (int k=0;k<m-1;k++)
-                {
-                    index++;
-                    if(index == tempvec.end())
-                        index = tempvec.begin();
-                }*/
-                tempvec.erase(index);
+                //vector<int>::iterator index = tempvec.begin() + m-1;
+                //if (*index > tempvec.size()) *index %= tempvec.size();
+                //tempvec.erase(index);
+                if (m > tempvec.size())  m %= tempvec.size();
+                tempvec.erase(tempvec.begin() +m);
             }            
-            //int* pos = tempvec.data();//pointer to the first element
-            //cout << *pos << ' ';
             cout << tempvec.front() << ' ';
         }
         cout << '\n';
